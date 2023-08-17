6772db0129e39f4e44738316a7a6cc926c8e2e22
mthanh2209 <mongthanh.220902@gmail.com>
2023-08-17 17:35:20 +0700

    feat(js_example): update example with object

--- objects.js
+++ objects.js
@@ -20,3 +20,16 @@ const car = {
   },
 };
 car.start();
+
+//obj propertype: thêm thuộc tính bên ngoài hàm tạo
+//khi output thì kh nhận đc giá trị truyền vào
+function Animal(name, leg, speed) {
+  (this.name = name), (this.leg = leg), (this.speed = speed);
+}
+Animal.prototype.sex = "male";
+
+var animal1 = new Animal("dog", 4, "fast");
+var animal2 = new Animal("turtle", 4, "slow");
+
+console.log(animal1.sex);
+console.log(animal2);
