210dc8d8df41933e99fc8c86595c847a28bef5bf
mthanh2209 <mongthanh.220902@gmail.com>
2023-08-23 11:30:55 +0700

    feat(js_example): update example with object

--- objects.js
+++ objects.js
@@ -33,3 +33,12 @@ var animal2 = new Animal("turtle", 4, "slow");
 
 console.log(animal1.sex);
 console.log(animal2);
+
+//Date object
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+
+console.log(`${day}/${month}/${year}`);
