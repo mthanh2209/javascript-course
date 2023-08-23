817630e148dad791f803ffcd46b959587069c323
mthanh2209 <mongthanh.220902@gmail.com>
2023-08-23 17:12:30 +0700

    feat(dom_example): create event handler example

---
+++ event-handler/events.js
@@ -0,0 +1,10 @@
+// Function to modify the text content of the paragraph
+const changeText = () => {
+  const p = document.querySelector("p");
+
+  p.textContent = "I changed because of an event handler property.";
+};
+
+// Add event handler as a property of the button element
+const button = document.querySelector("button");
+button.onclick = changeText;
