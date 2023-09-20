a9aafcd9ae6f5a3f3d6f08893f83e0d3ee48c5ae
mthanh2209 <mongthanh.220902@gmail.com>
2023-09-20 09:43:43 +0700

    feat: init MVC

---
+++ javascript-practice/src/javascripts/app.js
@@ -0,0 +1,15 @@
+import View from "./views/view";
+import Model from "./models/model";
+import Controller from "./controllers/controller";
+
+export class App {
+	constructor() { }
+
+	/**
+	 * Starts the app by initializing and triggering the controller's setup
+	 */
+	start() {
+		const controller = new Controller(new Model(), new View());
+		controller.init();
+	}
+}
