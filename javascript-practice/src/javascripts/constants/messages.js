c2baf51ede5db1acc0d7f7834ce3a7304ab23d9f
mthanh2209 <mongthanh.220902@gmail.com>
2023-09-28 15:30:26 +0700

    feat: fix naming for error message

--- javascript-practice/src/javascripts/constants/messages.js
+++ javascript-practice/src/javascripts/constants/messages.js
@@ -2,8 +2,8 @@
  * Message for alert the error while fetching or rendering.
  */
 export const ERROR_MESSAGE = {
-	GET_DATA: "Couldn't load data",
-	ADD_CART: "Couldn't add to cart",
+	LOAD_ERROR: "Couldn't load data",
+	ADD_TO_CART: "Couldn't add to cart",
 	SERVER_ERROR: "Server Error! Please try again later",
 };
 
@@ -11,5 +11,5 @@ export const ERROR_MESSAGE = {
  * Message for alert if the action be done successfully.
  */
 export const SUCCESS_MESSAGE = {
-	ADD_CART: "Add to cart successfully",
+	ADD_TO_CART: "Add to cart successfully",
 };
