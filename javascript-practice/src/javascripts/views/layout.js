e6425928b37e6b707a07eac74cf599af20e19223
mthanh2209 <mongthanh.220902@gmail.com>
2023-10-01 22:29:26 +0700

    feat: update category view

--- javascript-practice/src/javascripts/views/layout.js
+++ javascript-practice/src/javascripts/views/layout.js
@@ -1,10 +1,11 @@
 import Header from "../template/header";
 import Footer from "../template/footer";
+import APICategory from "../services/category";
 
 class LayoutView {
 	constructor() {
-		this.headerEl = document.querySelector('.main-header');
-		this.footerEl = document.querySelector('.main-footer');
+		this.headerEl = document.querySelector(".main-header");
+		this.footerEl = document.querySelector(".main-footer");
 		this.renderHeader();
 		this.renderFooter();
 	}
@@ -15,8 +16,11 @@ class LayoutView {
 	 * Renders a header element.
 	 * @param {Object} header  - header object to render.
 	 */
-	renderHeader() {
-		const headerTemplate = Header.renderHeader;
+	renderHeader = async () => {
+		const categories = new APICategory();
+		// Fetch the list of categories from the API.
+		const list = (await categories.get()).data;
+		const headerTemplate = Header.renderHeader(list);
 
 		// Append headerTemplate to the header element.
 		this.headerEl.innerHTML = headerTemplate;
@@ -31,7 +35,7 @@ class LayoutView {
 
 		// Append footerTemplate to the footer element.
 		this.footerEl.innerHTML = footerTemplate;
-	};
+	}
 }
 
 export default LayoutView;
--- javascript-practice/src/javascripts/views/view.js
+++ javascript-practice/src/javascripts/views/view.js
@@ -1,10 +1,8 @@
 import LayoutView from "./layout";
-import CategoryView from "./category";
 
 class View {
 	constructor() {
 		this.layout = new LayoutView();
-		this.category = new CategoryView()
 	}
 }
 
