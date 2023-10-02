c1dbd985e08f1a215745971493d3aacc2235449e
mthanh2209 <mongthanh.220902@gmail.com>
2023-10-02 15:28:22 +0700

    feat: update error message

--- javascript-practice/src/javascripts/constants/messages.js
+++ javascript-practice/src/javascripts/constants/messages.js
@@ -2,8 +2,8 @@
  * Message for alert the error while fetching or rendering.
  */
 export const ERROR_MESSAGE = {
-	LOAD_ERROR: "Couldn't load data",
-	ADD_TO_CART: "Couldn't add to cart",
+	LOAD_ERROR: "Error when loading data",
+	ADD_TO_CART: "Error when adding cart",
 	SERVER_ERROR: "Server Error! Please try again later",
 };
 
