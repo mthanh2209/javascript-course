6c65c4fcdbd8f7c35f6b287d4f3b6af86d59f201
mthanh2209 <mongthanh.220902@gmail.com>
2023-10-09 10:25:50 +0700

    feat: rename api service for product model

--- javascript-practice/src/javascripts/models/product.js
+++ javascript-practice/src/javascripts/models/product.js
@@ -1,13 +1,13 @@
-import APIHelper from "../services/helpers"
+import APIService from "../services/service"
 
 export default class ProductModel {
 	constructor() {
-		this.apiHelper = new APIHelper("/products")
+		this.apiService = new APIService("/products")
 	}
 
 	/**
 	 * Method to retrieve the list of product from the API.
 	 * @returns {Promise<Object[]>} A promise that resolves with the list of product.
 	 */
-	getProductList = () => this.apiHelper.getList()
+	getProductList = () => this.apiService.getList()
 }
