529fca0086281e02c609f4865ce9c8ab89fa681a
mthanh2209 <mongthanh.220902@gmail.com>
2023-10-09 15:25:31 +0700

    feat: remove call model in view

--- javascript-practice/src/javascripts/views/layout.js
+++ javascript-practice/src/javascripts/views/layout.js
@@ -1,28 +1,23 @@
 import Header from "../template/header";
 import Footer from "../template/footer";
-import CategoryModel from "../models/category";
 
 class LayoutView {
 	constructor() {
-		this.categoryModel = new CategoryModel()
 		this.headerEl = document.querySelector(".main-header");
 		this.footerEl = document.querySelector(".main-footer");
-		this.renderLayout();
 	}
 
-	//----- RENDERING -----//
-
-	/*
+	/**
 	 * Renders the header and footer elements.
 	 * This function fetches the list of categories from the API and renders the header
 	 * and footer templates using the retrieved data.
+	 *
+	 * @param {Function} getCategoryList - A function to fetch the list of categories from the API.
 	 */
-	renderLayout = async () => {
-		const categories = await this.categoryModel.getCategoryList();
-
+	renderLayout = (getCategoryList) => {
 		// Render the header and footer templates.
-		const headerTemplate = Header.renderHeader(categories);
-		const footerTemplate = Footer.renderFooter(categories);
+		const headerTemplate = Header.renderHeader(getCategoryList);
+		const footerTemplate = Footer.renderFooter(getCategoryList);
 
 		// Append the templates to their respective elements.
 		this.headerEl.innerHTML = headerTemplate;
