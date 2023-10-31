8af6999c196409c2506b704d59c07a524dae3c17
mthanh2209 <mongthanh.220902@gmail.com>
2023-10-31 21:54:03 +0700

    feat: build vercel

--- javascript-practice/src/javascripts/constants/url.js
+++ javascript-practice/src/javascripts/constants/url.js
@@ -2,7 +2,6 @@
  * Constants defining basic URL paths in the application
  */
 export const API_URL = "http://localhost:3000";
-export const BASE_URL = "http://localhost:1234";
-export const DETAIL_PAGE = `${BASE_URL}/detail.html`;
-export const PRODUCT_LISTING_PAGE = `${BASE_URL}/product-listing.html`;
-export const SHOPPING_CART_PAGE = `${BASE_URL}/shopping-cart.html`;
+export const DETAIL_PAGE = "/detail.html";
+export const PRODUCT_LISTING_PAGE = "/product-listing.html";
+export const SHOPPING_CART_PAGE = "/shopping-cart.html";
