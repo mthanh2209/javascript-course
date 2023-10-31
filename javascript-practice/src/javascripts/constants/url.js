92ae9a99527a5ce357e2b6d9db9191910c8effa9
mthanh2209 <mongthanh.220902@gmail.com>
2023-10-31 23:24:25 +0700

    feat: update api url

--- javascript-practice/src/javascripts/constants/url.js
+++ javascript-practice/src/javascripts/constants/url.js
@@ -1,7 +1,7 @@
 /**
  * Constants defining basic URL paths in the application
  */
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://javascript-practice-an5k.onrender.com/";
 export const DETAIL_PAGE = "/detail.html";
 export const PRODUCT_LISTING_PAGE = "/product-listing.html";
 export const SHOPPING_CART_PAGE = "/shopping-cart.html";
